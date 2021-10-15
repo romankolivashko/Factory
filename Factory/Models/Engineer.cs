7a6a1478d7f9af4655fa900062dbf62d84624a31
Roman Kolivashko <rkolivashko@gmail.com>
2021-10-15 08:57:01 -0700

    Added models

--- Factory/Models/Engineer.cs
+++ Factory/Models/Engineer.cs
@@ -1,9 +1,21 @@
 using System.Collections.Generic;
+using System;
 
-namespace Factory
+namespace Factory.Models
 {
-  public class Engineer
-  {
-    // properties, constructors, methods, etc. go here
-  }
+    public class Engineer
+    {
+        public Engineer()
+        {
+            this.JoinEntities = new HashSet<MachineEngineer>();
+            //this.Completed = false;
+        }
+
+        public int EngineerId { get; set; }
+        public string Description { get; set; }
+        //public bool Completed { get; set; }
+        public DateTime Hired {get;set;}
+
+        public virtual ICollection<MachineEngineer> JoinEntities { get; set; }
+    }
 }
--- Factory/Models/Machine.cs
+++ Factory/Models/Machine.cs
@@ -1,9 +1,16 @@
 using System.Collections.Generic;
 
-namespace Factory
+namespace FalloutFactory.Models
 {
   public class Machine
   {
-    // properties, constructors, methods, etc. go here
+    public Machine()
+    {
+        this.JoinEntities = new HashSet<MachineEngineer>();
+    }
+
+    public int MachineId { get; set; }
+    public string Name { get; set; }
+    public virtual ICollection<MachineEngineer> JoinEntities { get; set; }
   }
 }
